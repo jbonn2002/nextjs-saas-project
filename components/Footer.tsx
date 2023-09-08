31ba704367bf9daf9c08900d4eb6732a5519450a
Jordy <jbonn2002@gmail.com>
2023-09-08 12:31:30 -0700

    small changes to footer, added target blank to all a tags, and changed links to appropriate destination urls

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,29 +10,32 @@ export const Footer = () => {
     <footer className="grid grid-cols-1 items-center gap-8 border-t border-neutral-900/50 px-6 pt-12 pb-20 sm:grid-cols-2 sm:py-12 sm:px-8 lg:grid-cols-3">
       <div className="flex flex-row items-center justify-center gap-6 text-sm text-white md:justify-start">
         <SystemStatusButton status={"operational"} />
-        <Link href="/legal/terms">Terms</Link>
-        <Link href="/legal/privacy">Privacy</Link>
+        <Link href="/">Terms</Link>
+        <Link href="/">Privacy</Link>
       </div>
       <div className="hidden flex-row items-baseline justify-center gap-1 text-center text-sm text-white lg:flex"></div>
       <div className="mr-0 flex flex-row items-center justify-center gap-4 text-white sm:mr-8 sm:justify-end md:mr-12 xl:mr-0">
         <a
           className="transition hover:text-neutral-500"
-          href="https://github.com/motifland/markprompt"
-          aria-label="Markprompt on GitHub"
+          href="https://github.com/jbonn2002/nextjs-saas-project"
+          aria-label="Exemplarium on GitHub"
+          target="_blank"
         >
           <GitHubIcon className="h-5 w-5" />
         </a>
         <a
           className="transition hover:text-neutral-500"
-          href="https://twitter.com/markprompt"
-          aria-label="Markprompt on Twitter"
+          href="https://twitter.com/jordybdev"
+          aria-label="Jordybdev on Twitter"
+          target="_blank"
         >
           <TwitterIcon className="h-5 w-5" />
         </a>
         <a
           className="transition hover:text-neutral-500"
-          href="https://discord.gg/MBMh4apz6X"
-          aria-label="Markprompt on Discord"
+          href="https://discord.com/"
+          aria-label="Discord"
+          target="_blank"
         >
           <DiscordIcon className="h-5 w-5" />
         </a>
